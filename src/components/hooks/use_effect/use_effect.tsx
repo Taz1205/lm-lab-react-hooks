5560a73d2e7946a7daf8c7f58afff2a65f4963b1
Fatima Zahra <fathima_z@yahoo.com>
2023-07-17 17:01:27 +0100

    Implemented useEffect hook

--- src/components/hooks/use_effect/use_effect.tsx
+++ src/components/hooks/use_effect/use_effect.tsx
@@ -1,14 +1,47 @@
-// make an api call using useEffect
+import { useState, useEffect } from "react";
 
-//import { useState, useEffect } from "react";
+interface Todo {
+  id: number;
+  title: string;
+  completed: boolean;
+}
 
 export const APICall = () => {
-  // Your code here!
+  const [todo, setTodo] = useState<Todo | null>(null);
+  const [loading, setLoading] = useState(true);
+
+  const jsonTypicode = "https://jsonplaceholder.typicode.com/todos/1";
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        setLoading(true);
+        const response = await fetch(jsonTypicode);
+        const content = await response.json();
+        setTodo(content);
+      } catch (error) {
+        console.log(error);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchData();
+  }, []);
+
+  if (loading) return "Loading...";
 
   return (
     <>
       <h2>useEffect</h2>
-      <p>Your todo here!</p>
+      {todo ? (
+        <div>
+          <p>{`Todo ID: ${todo.id}`}</p>
+          <p>{`Title: ${todo.title}`}</p>
+          <p>{`Completed: ${todo.completed}`}</p>
+        </div>
+      ) : (
+        <p>No todo fetched.</p>
+      )}
     </>
   );
 };
