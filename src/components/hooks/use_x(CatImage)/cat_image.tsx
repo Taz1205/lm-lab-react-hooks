9d06712769f7dfd8f53d6eac60ffcc070f39da15
Fatima Zahra <fathima_z@yahoo.com>
2023-07-27 20:09:56 +0100

    Tried out config.ts to hide my api key &works now

--- src/components/hooks/use_x(CatImage)/cat_image.tsx
+++ src/components/hooks/use_x(CatImage)/cat_image.tsx
@@ -1,11 +1,12 @@
 import { useTodo } from "../use_x/use_todo";
+import { environment } from "../../../config/config";
 
 interface CatResponse {
   url: string;
 }
 
 export const CatImage = () => {
-  const url = `https://api.thecatapi.com/v1/images/search?api_key=live_4HDV1u3sSP3TaZpYhh8TtilgvxC8mOakCG84rfTY5HeFu4jBMBwY1pXK900wkA4D`;
+  const url = `https://api.thecatapi.com/v1/images/search?api_key=${environment.API_KEY}`;
   const { data, isFetching, error } = useTodo<CatResponse[]>(url);
 
   if (error) {
